4c0ac9348952b621c480def48e96cbce830f4a46
Cristian <albadancris1703@gmail.com>
2024-10-13 18:47:38 -0500

    login y barra unidos

--- app/src/main/java/com/appmoviles/utadeliciasapp/AuthActivity.kt
+++ app/src/main/java/com/appmoviles/utadeliciasapp/AuthActivity.kt
@@ -12,6 +12,10 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 
+enum class ProviderType {
+    BASIC
+}
+
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,38 +36,36 @@ class AuthActivity : AppCompatActivity() {
         setup()
     }
 
-    private  fun setup(){
-
-        title= "Ingresar"
+    private  fun setup() {
+        title = "Ingresar"
         val signUpButton = findViewById<Button>(R.id.signUpButton)
         val logInButton = findViewById<Button>(R.id.logOutbutton)
         val emailEditText = findViewById<EditText>(R.id.emailEditText)
         val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
 
-        signUpButton.setOnClickListener{
-            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+        signUpButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
                 FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString()).addOnCompleteListener {
-                    if (it.isSuccessful){
-                        showHome(it.result?.user?.email ?:"", ProviderType.BASIC)
-                    } else{
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
                         showAlert()
                     }
                 }
             }
         }
 
-        logInButton.setOnClickListener{
-            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+        logInButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
                 FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString()).addOnCompleteListener {
-                    if (it.isSuccessful){
-                        showHome(it.result?.user?.email ?:"", ProviderType.BASIC)
-                    } else{
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
                         showAlert()
                     }
                 }
             }
         }
-
     }
 
     private fun showAlert() {
@@ -75,12 +77,10 @@ class AuthActivity : AppCompatActivity() {
         dialog.show()
     }
 
-    private fun showHome(email: String, provider: ProviderType){
-
-        val homeIntent = Intent(this, HomeActivity::class.java).apply{
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
             putExtra("email", email)
             putExtra("provider", provider.name)
-
         }
         startActivity(homeIntent)
     }
--- app/src/main/java/com/appmoviles/utadeliciasapp/HomeActivity.kt
+++ app/src/main/java/com/appmoviles/utadeliciasapp/HomeActivity.kt
@@ -1,48 +1,51 @@
 package com.appmoviles.utadeliciasapp
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.auth.FirebaseAuth
+import androidx.fragment.app.Fragment
+import com.appmoviles.utadeliciasapp.databinding.ActivityNavegacionBinding
 
-enum class ProviderType{
-    BASIC
-}
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNavegacionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityNavegacionBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        //Setup
+
         val bundle = intent.extras
         val email = bundle?.getString("email")
         val provider = bundle?.getString("provider")
-        setup(email ?: "", provider ?: "")
-    }
 
-    private fun setup(email: String, provider: String) {
+        replaceFragment(inicio_fragmento())
 
-        val emailTextView = findViewById<TextView>(R.id.emailTextView)
-        val providerTextView = findViewById<TextView>(R.id.providerTextView)
-        val logOutButton = findViewById<TextView>(R.id.logOutbutton)
-
-        title="Menú Principal"
-        emailTextView.text = email
-        providerTextView.text = provider
-
-        logOutButton.setOnClickListener{
-            FirebaseAuth.getInstance().signOut()
-            onBackPressed()
+        binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> replaceFragment(inicio_fragmento())
+                R.id.notification -> replaceFragment(notificaciones_fragmento())
+                R.id.productos -> replaceFragment(productos_fragmentos())
+                R.id.descuentos -> replaceFragment(cupones_fragmento())
+                R.id.settings -> replaceFragment(ajustes_fragmento.newInstance(email ?: "", provider ?: ""))
+                else -> false
+            }
+            true
         }
     }
 
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
 }
