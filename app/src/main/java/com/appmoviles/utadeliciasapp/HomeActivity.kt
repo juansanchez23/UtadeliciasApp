faa21354716bd2825ef2a75619907f1622ac5fc1
Sir_Zedrak <andresf.gacharnat@utadeo.edu.co>
2024-10-13 11:41:51 -0500

    Union-Login-MenuBar- Falta Logout

--- app/src/main/java/com/appmoviles/utadeliciasapp/HomeActivity.kt
+++ app/src/main/java/com/appmoviles/utadeliciasapp/HomeActivity.kt
@@ -7,26 +7,57 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
+import com.appmoviles.utadeliciasapp.databinding.ActivityNavegacionBinding
 
 enum class ProviderType{
     BASIC
 }
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNavegacionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+
+        setContentView(R.layout.fragment_ajustes)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         //Setup
         val bundle = intent.extras
         val email = bundle?.getString("email")
         val provider = bundle?.getString("provider")
         setup(email ?: "", provider ?: "")
+
+        binding = ActivityNavegacionBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        replaceFragment(inicio_fragmento())
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.home -> replaceFragment(inicio_fragmento())
+                R.id.notification -> replaceFragment(notificaciones_fragmento())
+                R.id.productos -> replaceFragment(productos_fragmentos())
+                R.id.descuentos -> replaceFragment(cupones_fragmento())
+                R.id.settings -> replaceFragment(ajustes_fragmento())
+
+                else ->{
+
+                }
+            }
+            true
+        }
     }
 
     private fun setup(email: String, provider: String) {
@@ -43,6 +74,14 @@ class HomeActivity : AppCompatActivity() {
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager= supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 
 }
