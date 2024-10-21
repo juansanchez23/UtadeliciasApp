2633098488eb9f97c776763cdb943c4b264dd7ec
Juan Sebastian Sanchez Rodriguez <juans.sanchezro@utadeo.edu.co>
2024-10-20 21:35:31 -0500

    products update

--- app/src/main/java/com/appmoviles/utadeliciasapp/AddProductsFragment.kt
+++ app/src/main/java/com/appmoviles/utadeliciasapp/AddProductsFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AddProductFragment.newInstance] factory method to
+ * Use the [AddProductsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AddProductFragment : Fragment() {
+class AddProductsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class AddProductFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_product, container, false)
+        return inflater.inflate(R.layout.fragment_add_products, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class AddProductFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AddProductFragment.
+         * @return A new instance of fragment AddProductsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AddProductFragment().apply {
+            AddProductsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
