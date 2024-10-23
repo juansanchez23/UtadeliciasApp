f380a20c9953501fb1ef06d2bb6aa65a98a230bb
Sir_Zedrak <andresf.gacharnat@utadeo.edu.co>
2024-10-22 22:46:49 -0500

    SCANNER DE QR Y LOGO CORREGIDO

--- app/src/main/java/com/appmoviles/utadeliciasapp/Scanner.kt
+++ app/src/main/java/com/appmoviles/utadeliciasapp/Scanner.kt
@@ -1,59 +1,103 @@
 package com.appmoviles.utadeliciasapp
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import com.budiyev.android.codescanner.CodeScanner
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.appmoviles.utadeliciasapp.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Scanner.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Scanner : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var codeScanner: CodeScanner
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        // Infla el layout para este fragmento
         return inflater.inflate(R.layout.fragment_scanner, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Scanner.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Scanner().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+
+        val scannerView = view.findViewById<CodeScannerView>(R.id.scanner_view)
+
+        // Inicializa CodeScanner
+        codeScanner = CodeScanner(requireContext(), scannerView)
+
+        // Configura los parámetros
+        codeScanner.camera = CodeScanner.CAMERA_BACK // o CAMERA_FRONT
+        codeScanner.formats = CodeScanner.ALL_FORMATS
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE
+        codeScanner.isAutoFocusEnabled = true
+        codeScanner.isFlashEnabled = false
+
+        // Callbacks
+        codeScanner.decodeCallback = DecodeCallback {
+            requireActivity().runOnUiThread {
+                Toast.makeText(requireContext(), "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
             }
+        }
+        codeScanner.errorCallback = ErrorCallback {
+            requireActivity().runOnUiThread {
+                Toast.makeText(requireContext(), "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+
+        // Inicia el escaneo cuando se toca el scannerView
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+
+        val seekBar = view.findViewById<SeekBar>(R.id.seekBar)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                codeScanner.zoom=progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Manejo cuando empieza a tocar
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Manejo cuando deja de tocar
+            }
+        })
+
+        checkPermission(android.Manifest.permission.CAMERA,200 )
     }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview() // Reinicia el preview al volver
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources() // Libera recursos de la cámara al pausar
+        super.onPause()
+    }
+
+    fun checkPermission(permission: String, reqCode: Int) {
+        if (ContextCompat.checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(permission), reqCode)
+        }
+    }
+
 }
