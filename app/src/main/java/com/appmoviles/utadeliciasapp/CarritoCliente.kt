1bb6baf7faea5d5797ed9da50ec7771dda20c655
gabis260 <gabigonza260@gmail.com>
2024-10-31 08:06:24 -0500

    actualizacion de la nav

--- app/src/main/java/com/appmoviles/utadeliciasapp/CarritoCliente.kt
+++ app/src/main/java/com/appmoviles/utadeliciasapp/CarritoCliente.kt
@@ -6,13 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
-class home_cliente : Fragment() {
+class CarritoCliente : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home_cliente, container, false)
