bb4932efb949f27731a1b883eb21aa025332a5f0
Sir_Zedrak <andresf.gacharnat@utadeo.edu.co>
2024-11-05 10:56:21 -0500

    KJASHDJKASHDKJASHDJKJ

--- app/src/main/java/com/appmoviles/utadeliciasapp/CarritoItem.kt
+++ app/src/main/java/com/appmoviles/utadeliciasapp/CarritoItem.kt
@@ -1,7 +1,7 @@
 package com.appmoviles.utadeliciasapp
 
 data class CarritoItem(
-    val producto: Products = Products(), // Producto con un constructor vacío en Products
+    var producto: Products = Products(), // Producto con un constructor vacío en Products
     var selectedQuantity: Int = 1
 ) {
     // Agrega un constructor sin argumentos para Firebase
